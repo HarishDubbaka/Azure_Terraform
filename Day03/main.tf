c67696b110d60e3038f7beb869132fb334c3ea49
DUBBAKA HARISH <166729357+HarishDubbaka@users.noreply.github.com>
2025-12-03 05:06:39 +0530

    Update main.tf

--- Day03/main.tf
+++ Day03/main.tf
@@ -1,1 +1,31 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.8.0"
+    }
+  }
+  required_version = ">=1.10.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "South India"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "dubbakas"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location  # implicit dependency
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
 
