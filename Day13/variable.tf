4d05b9aa0b7d46b2ee314d23a7bd038e19123321
DUBBAKA HARISH <166729357+HarishDubbaka@users.noreply.github.com>
2025-12-13 09:07:37 +0530

    Add variables for resource group and locations
    Define variables for project name and allowed locations.

---
+++ Day13/variable.tf
@@ -0,0 +1,12 @@
+variable "project_name" {
+  description = "The name of the resource group"
+  type        = string
+  default     = "Dubbaka_Resourcegroup"
+
+}
+
+variable "allowed_locations" {
+  description = "List of allowed locations for resource deployment"
+  type        = list(string)
+  default     = ["East US", "West Europe", "South India"]
+}
