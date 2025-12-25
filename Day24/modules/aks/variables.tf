5e9123bfcae4c20eeaf83fb1e97d2bff14642a84
DUBBAKA HARISH <166729357+HarishDubbaka@users.noreply.github.com>
2025-12-25 19:40:31 +0530

    Create variables.tf

---
+++ Day24/modules/aks/variables.tf
@@ -0,0 +1,6 @@
+variable "name" {}
+variable "location" {}
+variable "resource_group_name" {}
+variable "dns_prefix" {}
+variable "node_count" { default = 1 }
+variable "vm_size" { default = "Standard_DS2_v2" }
